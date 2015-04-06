f8888244aa0d553e17270d745c8b79c8de33cf56
AlexsJones <jonesax@hush.com>
2015-04-06 01:26:58 +0100

    adding comments and removing non public api on session

--- src/session/session.h
+++ src/session/session.h
@@ -48,25 +48,27 @@ typedef struct session {
   RSA *keypair;
 }session;
 
-void default_session_callback(void *gui_context, jnx_guid *session_guid,
-    jnx_char *decrypted_message);
-
+/*
+ *@fn session_state session_message_write(sesssion *s, jnx_uint8 *message)
+ *@brief writes a message that goes through the encryption layer to a remote session peer (if active)
+ *@param s is the session pointer
+ *@param message is the message to send
+ *@return session_state should be SESSION_STATE_OKAY on success
+ */
 session_state session_message_write(session *s,jnx_uint8 *message);
-
+/*
+ *@fn jnx_int session_message_read(session *s, jnx_uint8 **omessage);
+ *@brief blocks until it has read data from the secure_socket
+ *@param s is the session pointer
+ *@param omessage is a double pointer that mallocs and populates with buffer data (NULL if empty)
+ *@return bytes read (-1 on failure)
+ */
 jnx_int session_message_read(session *s, jnx_uint8 **omessage);
-
-session_state session_message_read_and_decrypt(session *s, jnx_uint8 *message,
-    jnx_uint8 **omessage);
-
+/*
+ *@fn jnx_int session_is_active(session *s)
+ *@brief this function is a quick way of determining if you can write to the remote session socket, usually polled after linking
+ *@param s is the session pointer
+ *@return 1 on active 0 on inactive
+ */
 jnx_int session_is_active(session *s);
-
-void session_add_initiator_public_key(session *s, jnx_char *key);
-
-void session_add_receiver_public_key(session *s, jnx_char *key);
-
-void session_add_shared_secret(session *s, jnx_uint8 *secret);
-
-void session_add_secure_comms_port(session *s, jnx_char *comms_port);
-
-void session_add_remote_peer_guid(session *s, jnx_uint8 *guid_str);
 #endif
