1b62bd0cb41dea8fa391a108a55b56a7c93d9cf1
Alex Jones <jonesax@hush.com>
2015-04-21 15:50:56 +0100

    updated

--- test/test_secure_comms.c
+++ test/test_secure_comms.c
@@ -3,15 +3,15 @@
  *
  *       Filename:  test_session.c
  *
- *    Description:  
+ *    Description:
  *
  *        Version:  1.0
  *        Created:  02/09/2015 05:57:10 AM
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
- *   Organization:  
+ *         Author:  YOUR NAME (),
+ *   Organization:
  *
  * =====================================================================================
  */
@@ -55,13 +55,13 @@ void test_secure_comms_receiver() {
   session_service *service = session_service_create();
   session *os;
   session_state e = session_service_create_session(service,&os);
-  JNXCHECK(session_service_session_is_linked(service,&os->session_guid) == 0); 
+  JNXCHECK(session_service_session_is_linked(service,&os->session_guid) == 0);
   //Lets generate the guid of some remote session
   jnx_guid h;
   jnx_guid g;
   jnx_guid_create(&h);
   jnx_guid_create(&g);
- 
+
   peer *l = peer_create(h,"127.0.0.1","Alex", 10);
   peer *n = peer_create(g,"127.0.0.1","Bob", 10);
   e = session_service_link_sessions(service,
@@ -70,7 +70,7 @@ void test_secure_comms_receiver() {
       &os->session_guid,l,n);
   JNXCHECK(e == SESSION_STATE_OKAY);
   JNXCHECK(linking_did_use_functor);
-  JNXCHECK(session_service_session_is_linked(service,&os->session_guid) == 1); 
+  JNXCHECK(session_service_session_is_linked(service,&os->session_guid) == 1);
 
   //Fake data required for the session
   os->is_connected = 1;
@@ -78,14 +78,14 @@ void test_secure_comms_receiver() {
 
   peerstore *store = peerstore_init(l, 0);
   peerstore_store_peer(store,n);
-  
+
   discovery_service *ds = discovery_service_create(1234, AF_INET, baddr, store);
 
   dtostr *d = malloc(sizeof(dtostr));
   d->sess = os;
   d->p = n;
   d->port = os->secure_comms_port;
-  
+
   fire_threaded_tcp_packet(d);
   secure_comms_start(SC_RECEIVER,ds,os,AF_INET);
 
