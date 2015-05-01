68b1cee649378520a13135aa7aff47185da2ed04
Alex Jones <jonesax@hush.com>
2015-05-01 08:33:35 +0100

    adding some comments

--- src/net/auth_comms.h
+++ src/net/auth_comms.h
@@ -25,17 +25,52 @@ typedef struct auth_comms_service {
   accept_reject_callback ar_callback;
 }auth_comms_service;
 
+/*
+ * @fn auth_comms_service *auth_comms_create()
+ * @brief creates the auth_comms_service
+ * @return a pointer to auth_comms_service struct
+ */
 auth_comms_service *auth_comms_create();
-
+/*
+ * @fn auth_comms_listener_start(auth_comms_service *ac,
+    discovery_service *ds,session_service *ss,void *linking_args)
+ * @brief starts a listener for auth_comms protobuf objects
+ * @param ac is a pointer to initialised auth_comm_service
+ * @param dc is a pointer to initialised discovery_service
+ * @param ss is a pointer to initialised session_service
+ * @param linking_args are any arguments that should be returned to user space during the
+ * receiver linking process
+ */
 void auth_comms_listener_start(auth_comms_service *ac,
     discovery_service *ds,session_service *ss,void *linking_args);
-
+/*
+ * @fn auth_comms_destroy(auth_comms_service **ac)
+ * @brief destroys the auth_comms_service
+ * @param ac is a double pointer to auth_comms_service
+ */
 void auth_comms_destroy(auth_comms_service **ac);
-
+/*
+ * @fn auth_comms_initiator_start(auth_comms_service *ac, \
+    discovery_service *ds, port_control_service *ps,
+    session *s)
+    @brief starts the handshaking process between two peers
+    that eventually leads to secure comms and returns valid sock fd's
+    with full encryption to the session
+ * @param ac is a pointer to initialised auth_comm_service
+ * @param dc is a pointer to initialised discovery_service
+ * @param ps is a pointer to initialised port_control_service
+ * @param s is a pointer to the current session
+ */
 void auth_comms_initiator_start(auth_comms_service *ac, \
     discovery_service *ds, port_control_service *ps, 
     session *s);
-
+/*
+ * @fn auth_comms_stop(auth_comms_service *ac,session *s)
+ * @brief stops the current auth_comms session by closing sockets
+ * and releasing data
+ * @param ac is a pointer to initialised auth_comm_service
+ * @param s is a pointer to the current session
+ */
 void auth_comms_stop(auth_comms_service *ac,session *s);
 
 #endif /* !AUTH_COMMS_H */
