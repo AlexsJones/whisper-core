368a77ef44c26e80136faead81b242c58ae8bbea
Dragan Glumac <dragan.glumac@gmail.com>
2015-05-02 13:56:14 +0100

    Updated receiver.

--- test/integration_test/receiver/test_receiver.c
+++ test/integration_test/receiver/test_receiver.c
@@ -21,12 +21,13 @@
 #include "session_service.h"
 #include "auth_comms.h"
 #include "discovery.h"
+
 static char *baddr = NULL;
 
 
-int linking_test_procedure(session *s,linked_session_type session_type,
-    void *optargs) {
-  JNX_LOG(NULL,"Linking now the receiver session..");
+int linking_test_procedure(session *s, linked_session_type session_type,
+                           void *optargs) {
+  JNX_LOG(NULL, "Linking now the receiver session..");
   /*
      jnx_char *default_secure_comms = "6666";
      auth_comms_service *ac = auth_comms_create();
@@ -35,42 +36,45 @@ int linking_test_procedure(session *s,linked_session_type session_type,
      */
   return 0;
 }
-int unlinking_test_procedure(session *s,linked_session_type session_type,
-    void *optargs) {
+
+int unlinking_test_procedure(session *s, linked_session_type session_type,
+                             void *optargs) {
   return 0;
 }
+
 int app_accept_reject(discovery_service *ds, jnx_guid *initiator_guild,
-    jnx_guid *session_guid) {
+                      jnx_guid *session_guid) {
   return 0;
 }
+
 void test_receiver() {
-  JNX_LOG(NULL,"test_linking");
+  JNX_LOG(NULL, "test_linking");
   session_service *service = session_service_create(linking_test_procedure,
-      unlinking_test_procedure);
+                                                    unlinking_test_procedure);
 
   //Lets generate the guid of some remote session
   jnx_guid h;
   jnx_guid_create(&h);
 
-  peerstore *store = peerstore_init(local_peer_for_user("receiver_bob",10), 0);
+  peerstore *store = peerstore_init(local_peer_for_user("receiver_bob", 10), 0);
 
   get_broadcast_ip(&baddr);
 
   discovery_service *ds = discovery_service_create(1234, AF_INET, baddr, store);
 
-  discovery_service_start(ds,BROADCAST_UPDATE_STRATEGY);
+  discovery_service_start(ds, BROADCAST_UPDATE_STRATEGY);
 
   auth_comms_service *ac = auth_comms_create();
   ac->ar_callback = app_accept_reject;
-  ac->listener = jnx_socket_tcp_listener_create("9991",AF_INET,15);
-  auth_comms_listener_start(ac,ds,service,NULL);
+  ac->listener = jnx_socket_tcp_listener_create("9991", AF_INET, 15);
+  auth_comms_listener_start(ac, ds, service, NULL);
 
-  while(1) {
+  while (1) {
 
     jnx_list *olist = NULL;
 
-    if(session_service_fetch_all_sessions(service,
-          &olist) != SESSION_STATE_NOT_FOUND) {
+    if (session_service_fetch_all_sessions(service,
+                                           &olist) != SESSION_STATE_NOT_FOUND) {
 
       printf("Found a remote session...\n");
 
@@ -83,15 +87,18 @@ void test_receiver() {
       }
 
       char buffy[1024];
+      bzero(&buffy, 1024);
+
       int size = 0;
-      do {
+      while (size <= 0)
         size = session_message_read(s, &buffy);
-        printf("buffy -> %s\n", buffy);
-      } while (size <= 0);
-      jnx_list_destroy(&olist);
+      printf("size -> %d, buffy -> %s\n", size, buffy);
+      sleep(10);
     }
+    jnx_list_destroy(&olist);
   }
 }
+
 int main(int argc, char **argv) {
   test_receiver();
   return 0;
