5195581ebaea57705ec3c03eabe3ce940bbdcb2d
Dragan Glumac <dragan.glumac@gmail.com>
2015-05-16 12:17:38 +0100

    Test for name clash, mk1.

--- test/unit_test/test_peerstore.c
+++ test/unit_test/test_peerstore.c
@@ -40,6 +40,22 @@ peer *other_test_peer() {
   }
   return peer_create(guid, "127.0.0.2", "OtherUser", 10);
 }
+peer *bob1() {
+  jnx_guid guid;
+  int i;
+  for (i = 0; i < 16; i++) {
+    guid.guid[i] = i + 1;
+  }
+  return peer_create(guid, "127.0.0.3", "Bob", 10);
+}
+peer *bob2() {
+  jnx_guid guid;
+  int i;
+  for (i = 0; i < 16; i++) {
+    guid.guid[i] = i + 2;
+  }
+  return peer_create(guid, "127.0.0.4", "Bob", 10);
+}
 peerstore *create_test_peerstore() {
   peerstore *store = peerstore_init(local_test_peer(), 0);
   return store;
@@ -55,6 +71,20 @@ void test_peerstore_lookup_by_username() {
   int status = peers_compare(other, peerstore_get_local_peer(ps));
   JNXCHECK(status == PEERS_DIFFERENT);
   status = peers_compare(local, peerstore_get_local_peer(ps));
+  JNXCHECK(status == PEERS_EQUIVALENT);
+  peerstore_destroy(&ps);
+}
+void test_peerstore_username_clash() {
+  peerstore *ps = create_test_peerstore();
+  peer *b1 = bob1();
+  peer *b2 = bob2();
+
+  peerstore_store_peer(ps, b1);
+  peerstore_store_peer(ps, b2);
+
+  int status = peers_compare(b1, peerstore_lookup_by_username(ps, "Bob"));
+  JNXCHECK(status == PEERS_EQUIVALENT);
+  status = peers_compare(b2, peerstore_lookup_by_username(ps, "Bob-0203"));
   JNXCHECK(status == PEERS_EQUIVALENT);
   peerstore_destroy(&ps);
 }
@@ -75,7 +105,10 @@ void test_peerstore_lookup_for_inactive_username() {
 int main() {
   JNXLOG(0, "test_peerstore_lookup_by_username");
   test_peerstore_lookup_by_username();
-  
+
+  JNX_LOG(0, "test_peerstore_username_clash");
+  test_peerstore_username_clash();
+
   JNXLOG(0, "test_peerstore_lookup_for_non_existant_username");
   test_peerstore_lookup_for_non_existant_username();
   
