b04dc0d1897edc92f8ad1fb9de3ce869ef74887d
AlexsJones <jonesax@hush.com>
2015-07-14 17:58:32 +0100

    reworking inviter test

--- test/integration_test/inviter/test_inviter.c
+++ test/integration_test/inviter/test_inviter.c
@@ -17,54 +17,66 @@
  */
 #include <stdlib.h>
 #include <jnxc_headers/jnxsocket.h>
-#include <jnxc_headers/jnxthread.h>
 #include <jnxc_headers/jnxlog.h>
 #include <jnxc_headers/jnx_tcp_socket.h>
 #include "session_service.h"
-#include "port_control.h"
 #include "auth_comms.h"
 #include "discovery.h"
+
 static char *baddr = NULL;
 static char *interface = NULL;
-static auth_comms_service *ac = NULL;
-int linking_test_procedure(session *s,linked_session_type session_type,
+static auth_comms_service *ac;
+int accept_invite_callback(jnx_guid *session_guid) {
+
+  return 1;
+}
+int linking_test_procedure(session *s, linked_session_type session_type,
     void *optargs) {
-  if(session_type == E_AM_INITIATOR){
-    JNXCHECK(session_type == E_AM_INITIATOR);
-    JNXLOG(NULL,"Session hit linking procedure functor");
-    discovery_service *ds = (discovery_service*)optargs;
-    /* Adding port control service */
-
-    int init_port = rand() % 1000;
-
-    port_control_service *ps = port_control_service_create(8000 + 
-        init_port,
-        12341,1);
-
-    ac = auth_comms_create();
-
-    ac->listener = jnx_socket_tcp_listener_create("9991",AF_INET,15);
-  
-    /* Do nothing the session is fake */
-  }
+  JNXLOG(NULL, "Linking now the receiver session..");
+  /*
+     jnx_char *default_secure_comms = "6666";
+     auth_comms_service *ac = auth_comms_create();
+     ac->listener = jnx_socket_tcp_listener_create("9991",AF_INET,15);
+     auth_comms_initiator_start(ac,ds,os,default_secure_comms);
+     */
   return 0;
 }
-int unlinking_test_procedure(session *s,linked_session_type session_type,
+
+int unlinking_test_procedure(session *s, linked_session_type session_type,
     void *optargs) {
 
+  auth_comms_stop(ac,s);
   return 0;
 }
-void test_joiner() {
-  JNXLOG(NULL,"test_joiner");
+
+int app_accept_reject(discovery_service *ds, jnx_guid *initiator_guild,
+    jnx_guid *session_guid) {
+  return 0;
+}
+
+void test_receiver() {
+  JNXLOG(NULL, "test_linking");
   session_service *service = session_service_create(linking_test_procedure,
       unlinking_test_procedure);
-  peerstore *store = peerstore_init(local_peer_for_user("initiator_bob",10,interface), 0);
+
+  //Lets generate the guid of some remote session
+  jnx_guid h;
+  jnx_guid_create(&h);
+
+  peerstore *store = peerstore_init(local_peer_for_user("receiver_bob", 10,interface), 0);
 
   get_broadcast_ip(&baddr,interface);
-  printf("%s\n", baddr);
+
   discovery_service *ds = discovery_service_create(1234, AF_INET, baddr, store);
 
-  discovery_service_start(ds,BROADCAST_UPDATE_STRATEGY);
+  discovery_service_start(ds, BROADCAST_UPDATE_STRATEGY);
+
+  ac = auth_comms_create();
+  ac->ar_callback = app_accept_reject;
+  ac->invitation_callback = accept_invite_callback;
+  ac->listener = jnx_socket_tcp_listener_create("9991", AF_INET, 15);
+  auth_comms_listener_start(ac, ds, service, NULL);
+
 
   int remote_peers = 0;
   jnx_guid **active_guids;
@@ -88,9 +100,8 @@ void test_joiner() {
   /* create a fake session to invite the remote peer too */
   session *os;
   session_state e = session_service_create_session(service,&os);
- 
-  jnx_guid h;
-  
+
+
   jnx_guid_create(&h);
 
   peer *fake_peer = peer_create(h,"111.112.113.114","FakeJoe",0);
@@ -100,19 +111,59 @@ void test_joiner() {
 
   /* here we have a fake session that does nothing, but lets tell remote peer 
    * about it */
-  
+
   auth_comms_invite_send(ac,os,remote_peer);
 
+start:
+  while (1) {
 
-  auth_comms_destroy(&ac);
+    jnx_list *olist = NULL;
+
+    if (session_service_fetch_all_sessions(service,
+          &olist) != SESSION_STATE_NOT_FOUND) {
+
+      printf("-----------------------------------------------\n");
+      session *s = jnx_list_remove_front(&olist);
+
+      while (!session_is_active(s)){
+        sleep(1);
+      }
+
+      jnx_char *buffy = NULL;
+
+      int size = 0;
+      while (size <= 0) {
+        size = session_message_read(s, &buffy);
+      }
+      if(size) {
+        printf("size -> %d, buffy -> %s\n", size, buffy);
+
+        JNXCHECK(session_is_active(s) == 1);
+
+        session_service_unlink_sessions(service,E_AM_RECEIVER,
+            ds,&(*s).session_guid);
+
+        JNXCHECK(session_is_active(s) == 0);
+
+        session_service_destroy_session(service,&(*s).session_guid);
+
+        jnx_list_destroy(&olist);
+
+        break;
+      }    
+    }
+    if(olist)
+      jnx_list_destroy(&olist);
+  }
 
 }
+
 int main(int argc, char **argv) {
   if (argc > 1) {
     interface = argv[1];
     printf("using interface %s", interface);
   }
   JNXLOG_CREATE("logger.conf");
-  test_joiner();
+  test_receiver();
   return 0;
 }
