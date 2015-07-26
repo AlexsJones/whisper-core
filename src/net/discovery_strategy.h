a1b1307f8dcee7add388cf1c16b02dfd240b006b
Dragan Glumac <dragan.glumac@gmail.com>
2015-07-26 08:56:04 +0100

    First commit for discovery strategy API.

---
+++ src/net/discovery_strategy.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  discovery_strategy.h
+ *
+ *    Description:  Struct and accompanying functions that define the API for discovery
+ *                  implementations. The whisper-core comes with pre-canned strategies
+ *                  for discovering peers on the LAN and across the LAN boundaries, but
+ *                  users can implement their own in the shared libraries and load them
+ *                  at run time.
+ *
+ *        Version:  1.0
+ *        Created:  26/07/2015 08:50:06
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dragan Glumac (DG), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+typedef struct {
+  // TODO: Define the API
+} discovery_strategy;
